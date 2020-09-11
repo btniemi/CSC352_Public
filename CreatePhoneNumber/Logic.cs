6b7909a87454b5f920689ebd092333aad2771bde
Braedon Niemi <70549370+btniemi@users.noreply.github.com>
2020-09-11 17:28:02 -0600

    Added CreatePhoneNumber

---
+++ CreatePhoneNumber/Logic.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreatePhoneNumber
+{
+    class Logic
+    {
+    }
+}
---
+++ CreatePhoneNumber/UnitTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CreatePhoneNumber
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void SomeTest()
+        {
+            Assert.IsTrue(true);
+        }
+        [Test]
+        public void SomeOtherTest()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+
+    public static class UnitTestHelpers
+    {
+        // Helper Libraries for this class
+    }
+}
