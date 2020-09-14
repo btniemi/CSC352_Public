51f1adcd40c0d46f3557734c6407732cf9a2de63
Braedon Niemi <70549370+btniemi@users.noreply.github.com>
2020-09-14 17:36:24 -0600

    UpdatedCreatePhoneNumber

--- CreatePhoneNumber/Logic.cs
+++ CreatePhoneNumber/Logic.cs
@@ -1,10 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CreatePhoneNumber
 {
     class Logic
     {
+        internal static string TranslateToPhoneNumber(int[] numbers)
+        {
+            // Want (XXX) XXX-XXXX below solution is using a queue
+            //System.Collections.Generic.Queue<string> queue = new Queue<string>();
+
+            //queue.Enqueue("(");
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    queue.Enqueue(numbers[i].ToString());
+            //}
+            //queue.Enqueue(") ");
+            //for(int i = 3; i < 6; i++)
+            //{
+            //    queue.Enqueue(numbers[i].ToString());
+            //}
+            //queue.Enqueue("-");
+            //for(int i= 6; i<10; i++)
+            //{
+            //    queue.Enqueue(numbers[i].ToString());
+            //}
+
+            //StringBuilder sb = new StringBuilder();
+
+            //while(queue.Count != 0)
+            //{
+            //    sb.Append(queue.Dequeue());
+            //}
+
+            //return sb.ToString();
+
+
+            //string interpulation
+            return $"({numbers[0]}{numbers[1]}{numbers[2]}) {numbers[3]}{numbers[4]}{numbers[5]}-{numbers[6]}{numbers[7]}{numbers[8]}{numbers[9]}";
+
+
+
+        }
     }
 }
--- CreatePhoneNumber/UnitTests.cs
+++ CreatePhoneNumber/UnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.VisualStudio.TestPlatform.Common.Telemetry;
 using NUnit.Framework;
 
 namespace CreatePhoneNumber
@@ -6,20 +7,16 @@ namespace CreatePhoneNumber
     [TestFixture]
     public class UnitTests
     {
-        [Test]
-        public void SomeTest()
-        {
-            Assert.IsTrue(true);
-        }
-        [Test]
-        public void SomeOtherTest()
-        {
-            Assert.IsTrue(false);
-        }
-    }
+        [TestCase(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, "(000) 000-0000")]
+        [TestCase(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, "(012) 345-6789")]
 
-    public static class UnitTestHelpers
-    {
-        // Helper Libraries for this class
+        public void ValidInputs(int[] numbers, string expected)
+        {
+            // Act
+            string actual = Logic.TranslateToPhoneNumber(numbers);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
     }
 }
