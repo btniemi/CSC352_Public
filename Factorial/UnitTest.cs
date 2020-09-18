69b960aa58e68be903ac8f8638018fcda02dd9fa
Ace Olszowka <aolszowka@gmail.com>
2020-09-18 14:56:53 -0600

    Added Factorial and GCD Programs

---
+++ Factorial/UnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace Factorial
+{
+    [Parallelizable(ParallelScope.All)]
+    [TestFixture]
+    public class UnitTest
+    {
+        [Test]
+        public void factorial_test()
+        {
+            var expected = -1;
+            var actual = Logic.factorial(15000);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ GCD/UnitTests.cs
@@ -0,0 +1,21 @@
+﻿namespace GCD
+{
+    using NUnit.Framework;
+
+    [Parallelizable(ParallelScope.All)]
+    [TestFixture]
+    public class UnitTests
+    {
+        [TestCase(1, 2, 1)]
+        [TestCase(2, 4, 2)]
+        [TestCase(30, 45, 15)]
+        [TestCase(12962250, 12345, 12345)]
+        [TestCase(1,long.MaxValue, 1)]
+        public void gcd_test(long x, long y, long expected)
+        {
+            long actual = Logic.gcd(x, y);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
