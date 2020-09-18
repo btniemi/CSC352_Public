7e5e19367f05be65dade2ea06cca8d33d38237fd
Ace Olszowka <aolszowka@gmail.com>
2020-09-18 15:16:30 -0600

    Added Summation Example

---
+++ Summation/Logic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Summation
+{
+    public class Logic
+    {
+        public static int Summation(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Summation/UnitTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Summation
+{
+    using NUnit.Framework;
+
+    [Parallelizable(ParallelScope.All)]
+    [TestFixture]
+    public class UnitTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 3)]
+        [TestCase(5, 15)]
+        [TestCase(100, 5050)]
+        [TestCase(200000, -1)]
+        public void Summation_Test(int input, int expected)
+        {
+            int actual = Logic.Summation(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
