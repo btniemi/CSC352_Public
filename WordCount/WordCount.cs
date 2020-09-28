213edfe3773623f202eaacf972f4b091a66c7c52
Braedon Niemi <70549370+btniemi@users.noreply.github.com>
2020-09-28 13:45:35 -0600

    Attempt at Solving WordCount
    I honestly have not clus how to do this but I kind of understand some of the structures that are out there to copy. not sure what a Regex is but that was an example and i am a bit confused

--- WordCount/WordCount.cs
+++ WordCount/WordCount.cs
@@ -6,12 +6,40 @@ namespace WordCount
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using NUnit.Framework.Interfaces;
 
     public static class WordCount
     {
         public static IDictionary<string, int> CountWords(string phrase)
         {
-            throw new NotImplementedException();
+            char[] delimeters = new char[] { ' ', ',', '\n' };
+            string[] splitPhrase = phrase.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> CountTheOccurances = new Dictionary<string, int>();
+            for (int i = 0; i < splitPhrase.Length; i++)                                       // Loop the splited string  
+            {
+                if (CountTheOccurances.ContainsKey(splitPhrase[i]))                           // Check if word is already in dictionary update the count  
+                {
+                    int value = CountTheOccurances[splitPhrase[i]];
+                    CountTheOccurances[splitPhrase[i]] = value + 1;
+                }
+                else                                                                     // If we found the same word we just increase the count in the dictionary 
+                {
+                    CountTheOccurances.Add(splitPhrase[i], 1);
+                }
+            }
+
+            foreach (KeyValuePair<string, int> kvp in CountTheOccurances)
+            {
+                string v = $"Counts: {kvp.Value}for {kvp.Key}";
+                return v;
+            }
+
+
+
+
         }
     }
 }
