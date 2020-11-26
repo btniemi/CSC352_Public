65ea13f1ff83b72a44a041dfc9783bab55a6a907
Braedon Niemi <70549370+btniemi@users.noreply.github.com>
2020-11-25 17:23:10 -0700

    KATA stuff

---
+++ DetectPanagram/Kata.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution
+{
+    internal class Kata
+    {
+
+        internal static bool IsPangram(string v)
+        {
+            Dictionary<string, int> letterFrequency = new Dictionary<string, int>()
+            {
+                {"a", 0 },
+                {"b", 0 },
+                {"c", 0 },
+                {"d", 0 },
+                {"e", 0 },
+                {"f", 0 },
+                {"g", 0 },
+                {"h", 0 },
+                {"i", 0 },
+                {"j", 0 },
+                {"k", 0 },
+                {"l", 0 },
+                {"m", 0 },
+                {"n", 0 },
+                {"o", 0 },
+                {"p", 0 },
+                {"q", 0 },
+                {"r", 0 },
+                {"s", 0 },
+                {"t", 0 },
+                {"u", 0 },
+                {"v", 0 },
+                {"w", 0 },
+                {"x", 0 },
+                {"y", 0 },
+                {"z", 0 },
+            };
+
+            foreach(char currentChar in v)
+            {
+                string lowerChar = currentChar.ToString().ToLower();
+                if (letterFrequency.ContainsKey(lowerChar))
+                {
+                    letterFrequency[lowerChar]++;
+                }
+            }
+
+            //return !letterFrequency.Any(kvp => kvp.Value == 0);
+            return letterFrequency.Where(kvp => kvp.Value == 0).ToArray().Length == 0;
+        }
+    }
+}
---
+++ DetectPanagram/Tests.cs
@@ -0,0 +1,23 @@
+﻿namespace Solution
+{
+    using NUnit.Framework;
+
+    using System;
+
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase("The quick brown fox jumps over the lazy dog.", true)]
+        [TestCase("The dog is a man under those hairy cloths.", false)]
+        [TestCase("abcdefghijklmnopqrstuvwxyz", true)]
+        [TestCase("ABCDEFGHIJKLMNOPQRSTUVWXYZ", true)]
+
+
+        public void SampleTests(string testString, bool expected)
+        {
+            Assert.AreEqual(expected, Kata.IsPangram(testString));
+        }
+    }
+}
+
+
